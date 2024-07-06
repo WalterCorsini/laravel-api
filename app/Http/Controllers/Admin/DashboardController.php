aa231dda08692aac0965c1e26f5a756123fe46d2
WalterCorsini <corsini.walter@gmail.com>
2024-07-06 17:21:26 +0200

    add data of user in dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -6,13 +6,16 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Project;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index() {
+
+$user = Auth::user();
         //  don't work
         $latestProjects = Project::recent()->get();
-        return view('admin.dashboard',compact('latestProjects'));
+        return view('admin.dashboard',compact('user'));
         // don't work
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -5,7 +5,9 @@
         <div class="row justify-content-center">
             <div class="col-md-8 mt-4">
                 <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
+                    <div class="card-header">
+                        <strong>{{ __('Login -> Ok') }}</strong>
+                    </div>
 
                     <div class="card-body">
                         @if (session('status'))
@@ -14,7 +16,37 @@
                             </div>
                         @endif
 
-                        {{ __('You are logged in!') }}
+                        {{ __('Bentornato') }}
+                        {{ $user->name }} !!
+                    </div>
+                </div>
+                <div class="card mt-5">
+                    <div class="card-header">
+                        <strong>{{ __('Riepilogo dati utente') }}</strong>
+                    </div>
+
+                    <div class="card-body">
+                        <dt>
+                            Nome :
+                        </dt>
+                        <dd>
+                            {{$user->name}}
+                        </dd>
+
+                        <dt>
+                            Email :
+                        </dt>
+                        <dd>
+                            {{$user->email}}
+                        </dd>
+
+                        <dt>
+                            Creazione Profilo :
+                        </dt>
+                        <dd>
+                            {{$user->created_at}}
+                        </dd>
+
                     </div>
                 </div>
             </div>
